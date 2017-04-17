82cf9c34b6ea7f2a779c163071d7743d154737d7
marcelojunin <marcelojunin2010@hotmail.com>
2017-04-16 23:25:27 -0300

    16/04

---
+++ www/js/index.js
@@ -0,0 +1,30 @@
+angular.module('myApp', ['ui.router','angularTypewrite','angular-parallax'])
+.config(function($stateProvider, $urlRouterProvider){
+	$stateProvider
+	.state('home',{
+		url:'/',
+		templateUrl:'templates/home.html',
+		controller:'myController'
+	})
+	.state('contact',{
+		url:'/contact',
+		templateUrl:'templates/contact.html',
+		controller:'myController'
+	})
+	$urlRouterProvider.otherwise('/');
+})
+.controller('myController',['$scope', function($scope){
+ 
+    		$scope.stuff = [
+    		"Olá!", 
+    		"Está procurando um site, sistema ou aplicativo?", 
+    		"Que maravilha!!!",
+    		"Você veio ao lugar certo!", 
+    		"Meu nome é Marcelo, e eu posso te ajudar a conseguir seu objetivo!",
+    		"Sou desenvolvedor, e por meio desse site, venho te apresentar um resumo do meu portifólio.",
+    		"Desenvolvo sites e sistema com as tecnologias atuais do mercado.",
+    		"Entre em contato!",
+    		"Ligue para 21 982525286",
+    		"Não perca mais tempo e envie um e-mail para contato@marceloprogrammer.com..." 
+    		]
+}])
