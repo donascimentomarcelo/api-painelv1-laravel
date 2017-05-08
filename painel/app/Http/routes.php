e6a557666921d817f290b5e698c7f526d3ab4a2c
Marcelo <marcelojunin2010@hotmail.com>
2017-05-08 16:57:23 -0300

    login

--- painel/app/Http/routes.php
+++ painel/app/Http/routes.php
@@ -11,13 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','Auth\AuthController@getLogin');
 
 
 	Route::post('painel/email',['as' => 'painel.email', 'uses' => 'PainelController@email']);
 
-	Route::get('painel/index',['as' => 'painel.index', 'uses' => 'PainelController@index']);
+	Route::get('auth/login', 'Auth\AuthController@getLogin');
+	Route::post('auth/login', 'Auth\AuthController@postLogin');
+	Route::get('auth/logout', 'Auth\AuthController@getLogout');
+	Route::get('auth/register', 'Auth\AuthController@getRegister');
+	Route::post('auth/register', 'Auth\AuthController@postRegister');
 
-
+	Route::get('/home', function () {
+    return view('welcome');
+});
