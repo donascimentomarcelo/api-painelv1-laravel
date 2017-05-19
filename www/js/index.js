f6d0e712393c8909776f9babc4ad657185d0922f
Marcelo <marcelojunin2010@hotmail.com>
2017-05-19 16:36:19 -0300

    Factory

--- www/js/index.js
+++ www/js/index.js
@@ -31,99 +31,23 @@ angular.module('myApp', [
                      '$http',
                      'snackbar',
                      'Carousel',
+                     '$configAPIService',
                            function($scope, 
                                     $document, 
                                     $location, 
                                     $http, 
                                     snackbar,
-                                    Carousel){
+                                    Carousel,
+                                    $configAPIService){
  
-    		$scope.stuff = [
-        "Procura um Programador?", 
-        "Procura um Web Designer?", 
-        "Procura um aplicativo para a sua empresa?", 
-    		"Você veio ao lugar certo!", 
-    		];
+    		$scope.stuff = $configAPIService.getStuff();
      
-          $scope.menuConfig3 = {
-            "buttonWidth": 60,
-            "menuRadius": 180,
-            "color": "rgba(8, 8, 8, 0.57)",
-            "offset":25,
-            "textColor": "#ffffff",
-            "showIcons":true,
-            "onlyIcon":false,
-            "textAndIcon": true,
-            "gutter": {
-              "top": 130,
-              "right": 30,
-              "bottom": 30,
-              "left": 30
-            },
-            "angles": {
-              "topLeft": 0,
-              "topRight": 90,
-              "bottomRight": 180,
-              "bottomLeft": 270
-            }
-          };
+        $scope.menuConfig3 = $configAPIService.getMenuConfig();
 
-          $scope.menuItems = [{
-            "title": "inicio",
-            "color": "rgba(8, 8, 8, 0.79);",
-            "rotate": 0,
-            "show": 0,
-            "titleColor": "#fff",
-            "icon":{"color":"#fff","name":"fa fa-home","size": 30}
-          }, {
-            "title": "perfil",
-            "color": "rgba(51, 51, 51, 0.88)",
-            "rotate": 0,
-            "show": 0,
-            "titleColor": "#fff",
-            "icon":{"color":"#fff","name":"fa fa-user-circle-o","size": 30}
-          }, {
-            "title": "projeto",
-            "color": "rgba(85, 85, 85, 0.88)",
-            "rotate": 0,
-            "show": 0,
-            "titleColor": "#fff",
-            "icon":{"color":"#fff","name":"fa fa-file-code-o","size": 30}
-          }, {
-            "title": "contato",
-            "color": "rgba(153, 153, 153, 0.93)",
-            "rotate": 0,
-            "show": 0,
-            "titleColor": "#fff",
-            "icon":{"color":"#fff","name":"fa fa-envelope-open-o","size": 30}
-          }];
-
-         var inicio   = angular.element(document.getElementById('inicio')),
-             perfil   = angular.element(document.getElementById('perfil')),
-             projetos = angular.element(document.getElementById('projetos')),
-             contato  = angular.element(document.getElementById('contato'));
+        $scope.menuItems = $configAPIService.getMenuItems();
 
         $scope.onWingClick = function(wing){
-          if(wing.title === 'inicio')
-          {
-            $document.scrollTo(inicio, 0, 1000);
-          }
-          else if(wing.title === 'perfil')
-          {
-            $document.scrollTo(perfil, 0, 1000);
-          }
-          else if(wing.title === 'projeto')
-          {
-            $document.scrollTo(projetos, 0, 1000);
-          }
-          else if(wing.title === 'contato')
-          {
-            $document.scrollTo(contato, 0, 1000);
-          }
-          else
-          {
-            $document.scrollTo(inicio, 0, 1000);
-          }
+           return $configAPIService.getOnWingClick(wing);
        };
 
        $scope.sendEmail = function(data){
@@ -136,10 +60,8 @@ angular.module('myApp', [
               }, function(responseError){
                 ignoreLoadingBar: true;
                  snackbar.create("Houve um erro ao enviar o e-mail!");
-                 // delete $scope.data;
                 console.log(responseError);
               });
-
        };
 
        var loadProject = function(){
@@ -147,7 +69,6 @@ angular.module('myApp', [
         // var promise = $http.get('http://marceloprogrammer.com/api/api/project/list');
           promise.then(function (data){
             ignoreLoadingBar: true;
-            console.log(data.data.data);
             $scope.dataProjects = data.data.data;
           },
            function(responseError){
@@ -156,9 +77,7 @@ angular.module('myApp', [
           });
        };
        // $scope.way = $sce.trustAsResourceUrl('http://localhost:8000/uploads/project/');
-       $scope.getIframeSrc = function () {
-        return 'http://localhost:8000/uploads/project/';
-      };
+
       $scope.Carousel = Carousel;
        loadProject();
 
